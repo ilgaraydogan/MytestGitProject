be1cb975fabd7e83b03f4141d8a3d45a43cac63f
Bayram Ilgar Aydogan <ilgaraydogan@icloud.com>
2024-11-05 00:18:40 +0300

    Initial Commit

---
+++ MytestGitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MytestGitProject
+//
+//  Created by Bayram Ilgar Aydoğan on 5.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
